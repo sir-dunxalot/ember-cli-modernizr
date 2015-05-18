04701f925bd38f36ab8bedcffd8c7f1c70e2b7f4
Duncan Graham Walker <walkerdu@bc.edu>
2015-05-17 23:01:26 -0400

    Basic broccoli filter running through files

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+var filterFiles = require('./lib/filter-files');
+var modernizr = require('modernizr');
+
 /* jshint node: true */
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-modernizr'
+  name: 'ember-cli-modernizr',
+  inDevelopment: false,
+
+  included: function(app) {
+    var environment = app.env;
+    var inDevelopment = environment === 'development';
+
+    this._super.included(app);
+
+    if (inDevelopment) {
+      app.import(app.bowerDirectory + '/modernizr/modernizr.js');
+    }
+
+    this.inDevelopment = inDevelopment;
+  },
+
+  postprocessTree: function(type, workingTree) {
+    return filterFiles(workingTree);
+  },
 };
