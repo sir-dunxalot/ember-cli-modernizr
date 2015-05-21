e2ba4143f64b9c0109c5b6fd2cdd7dd6dacc8f33
Duncan Graham Walker <walkerdu@bc.edu>
2015-05-21 19:17:45 -0400

    Test environment setup with Mocha and Chai

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var defaultFor = require('./lib/utils/default-for');
+var defaultOptions = require('./lib/default-options');
 var filterFiles = require('./lib/filter-files');
 var merge = require('deepmerge');
 var modernizr = require('modernizr');
@@ -6,33 +7,7 @@ var modernizr = require('modernizr');
 /* jshint node: true */
 'use strict';
 
-var defaultOptions = {
-  customTests: [],
-  developmentPath: null,
 
-  extensibility: {
-    addtest: false,
-    prefixed: false,
-    teststyles: false,
-    testprops: false,
-    testallprops: false,
-    hasevents: false,
-    prefixes: false,
-    domprefixes: false,
-    cssclassprefix: ''
-  },
-
-  extra: {
-    shiv: true,
-    printshiv: false,
-    load: true,
-    mq: false,
-    cssclasses: true
-  },
-
-  uglify: true,
-  tests: [], // Tests you want to implicitly include
-};
 
 module.exports = {
   name: 'ember-cli-modernizr',
