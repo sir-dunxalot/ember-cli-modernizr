c0ff64fd2f44e452774f1aaf2f52ecd4965aee3a
Duncan Graham Walker <walkerdu@bc.edu>
2015-05-21 18:09:26 -0400

    Default options added

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
+var defaultFor = require('./lib/utils/default-for');
 var filterFiles = require('./lib/filter-files');
 var modernizr = require('modernizr');
+
+var defaultOptions = {
+  customTests: [],
+  developmentPath: null,
+
+  extensibility: {
+    addtest: false,
+    prefixed: false,
+    teststyles: false,
+    testprops: false,
+    testallprops: false,
+    hasevents: false,
+    prefixes: false,
+    domprefixes: false,
+    cssclassprefix: ''
+  },
+
+  extra: {
+    shiv: true,
+    printshiv: false,
+    load: true,
+    mq: false,
+    cssclasses: true
+  },
+
+  uglify: true,
+  tests: [], // Tests you want to implicitly include
+};
 
 /* jshint node: true */
 'use strict';
@@ -7,15 +36,25 @@ var modernizr = require('modernizr');
 module.exports = {
   name: 'ember-cli-modernizr',
   inDevelopment: false,
+  options: null,
 
   included: function(app) {
     var environment = app.env;
     var inDevelopment = environment === 'development';
+    var passedOptions = defaultFor(app.options.modernizr, {});
+    var deveplomentPath;
 
     this._super.included(app);
 
     if (inDevelopment) {
-      app.import(app.bowerDirectory + '/modernizr/modernizr.js');
+      deveplomentPath = defaultFor(
+        passedOptions.developmentPath,
+        app.bowerDirectory + '/modernizr/modernizr.js'
+      );
+
+      app.import(developmentPath);
+    } else {
+
     }
 
     this.inDevelopment = inDevelopment;
