1ddb62023508da5247131936ca07fe1db6fd2dd5
Duncan Graham Walker <walkerdu@bc.edu>
2015-06-01 18:34:30 -0400

    Extensibility and extras read fixtures test skeletons

---
+++ tests/acceptance/extensibility-test.js
@@ -0,0 +1,46 @@
+var chai = require('chai');
+var readFixture = require('../helpers/read-fixture');
+var root = process.cwd();
+
+/* Test helpers */
+
+var assert = chai.assert;
+
+describe('Acceptance - Extensibility', function() {
+
+  beforeEach(function() {
+    process.chdir(root);
+  });
+
+  it('detects multiple Modernizr.addTest methods', function() {
+    var content = readFixture('extensibility/add-test-multiple.js');
+  });
+
+  it('detects a single Modernizr.addTest method', function() {
+    var content = readFixture('extensibility/add-test-single.js');
+  });
+
+  it('detects the Modernizr._domPrefixes API', function() {
+    var content = readFixture('extensibility/dom-prefixes.js');
+  });
+
+  it('detects the Modernizr.hasEvent API', function() {
+    var content = readFixture('extensibility/has-event.js');
+  });
+
+  it('detects the Modernizr._prefixes API', function() {
+    var content = readFixture('extensibility/dom-prefixes.js');
+  });
+
+  it('detects the Modernizr.testAllProps API', function() {
+    var content = readFixture('extensibility/test-all-props.js');
+  });
+
+  it('detects the Modernizr.testProp API', function() {
+    var content = readFixture('extensibility/test-prop.js');
+  });
+
+  it('detects the Modernizr.testStyles API', function() {
+    var content = readFixture('extensibility/test-styles.js');
+  });
+});
---
+++ tests/acceptance/extras-test.js
@@ -0,0 +1,26 @@
+var chai = require('chai');
+var readFixture = require('../helpers/read-fixture');
+var root = process.cwd();
+
+/* Test helpers */
+
+var assert = chai.assert;
+
+describe('Acceptance - Extensibility', function() {
+
+  beforeEach(function() {
+    process.chdir(root);
+  });
+
+  it('detects the Modernizr.mq API', function() {
+    var content = readFixture('extras/media-queries.js');
+  });
+
+  it('detects a core Modernizr.load call', function() {
+    var content = readFixture('extras/core-load.js');
+  });
+
+  it('detects a non-core Modernizr.load call', function() {
+    var content = readFixture('extras/non-core-load.js');
+  });
+});
