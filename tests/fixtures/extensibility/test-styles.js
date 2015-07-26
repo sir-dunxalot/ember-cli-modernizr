03c139597828d962578c27cf6521e4902ce34606
Duncan Graham Walker <walkerdu@bc.edu>
2015-07-25 22:53:26 -0400

    JS regex update for Modernizr.inputtypes.time

--- tests/fixtures/extensibility/test-styles.js
+++ tests/fixtures/extensibility/test-styles.js
@@ -1,7 +1,7 @@
 (function() {
 
   Modernizr.testStyles('#modernizr { width: 9px; color: papayawhip; }', function(elem, rule){
-    Modernizr.addTest('width', elem.offsetWidth == 9);
+    return true;
   }, 2, ["video", "image"]);
 
 });
